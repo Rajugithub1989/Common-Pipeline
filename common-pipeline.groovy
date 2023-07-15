268603c2d963e7a8e8490e8dd1c6b5e358b2f474
rkbs <renukaraju.kbs@dxc.com>
2023-07-16 02:05:41 +0530

    Added script

---
+++ common-pipeline.groovy
@@ -0,0 +1,9 @@
+{ ->
+    node() {
+       git clone https://github.com/Rajugithub1989/java-hello-world-webapp-1.git
+       git clone https://github.com/Rajugithub1989/java-hello-world-webapp-2.git
+       git clone https://github.com/Rajugithub1989/java-hello-world-webapp-3.git            
+
+       // Whatever you do with your 3 repos...
+    }
+}
